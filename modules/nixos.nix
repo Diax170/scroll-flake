bf0eedeb7e6a21ed32ac480900e45a6c3b70a15c
OliMoli <olimoli@disroot.org>
2026-07-15 19:38:35 +0200

    fix: properly export SWAYSOCK

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -183,9 +183,9 @@ in
             "scroll/config.d/nixos.conf".source = pkgs.writeText "nixos.conf" ''
               # Import the most important environment variables into the D-Bus and systemd
               # user environments (e.g. required for screen sharing and Pinentry prompts):
-              exec dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SCROLLSOCK XDG_CURRENT_DESKTOP XDG_SESSION_TYPE PATH NIXOS_OZONE_WL XCURSOR_THEME XCURSOR_SIZE
+              exec dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY I3SOCK SWAYSOCK SCROLLSOCK XDG_CURRENT_DESKTOP XDG_SESSION_TYPE PATH NIXOS_OZONE_WL XCURSOR_THEME XCURSOR_SIZE
               # enable systemd-integration
-              exec "systemctl --user import-environment {,WAYLAND_}DISPLAY SCROLLSOCK; systemctl --user start scroll-session.target"
+              exec "systemctl --user import-environment {,WAYLAND_}DISPLAY I3SOCK SWAYSOCK SCROLLSOCK; systemctl --user start scroll-session.target"
               exec scrollmsg -t subscribe '["shutdown"]' && systemctl --user stop scroll-session.target
             '';
           }
